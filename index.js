09505a8f82af0c2b685a2e7c5872ee0a386d4dc6
mengwah0727 <mengwah1997@gmail.com>
2026-06-01 12:24:49 +0800

    Align temp voice controls

--- index.js
+++ index.js
@@ -218,9 +218,7 @@ function tempVoiceControlRows(channelId) {
       new ButtonBuilder()
         .setCustomId(`tempvoice_lock_${channelId}`)
         .setLabel('锁房')
-        .setStyle(ButtonStyle.Secondary)
-    ),
-    new ActionRowBuilder().addComponents(
+        .setStyle(ButtonStyle.Secondary),
       new ButtonBuilder()
         .setCustomId(`tempvoice_unlock_${channelId}`)
         .setLabel('解锁')
